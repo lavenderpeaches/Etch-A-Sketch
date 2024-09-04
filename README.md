02bf5e06d88701b004c8c83c8448d493e82dfefc
Deepali <deepalix7@gmail.com>
2024-09-05 00:19:48 +0530

    Updated the README and the final code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Etch-A-Sketch
-The fourth project of The Odin Project
+The fourth project of The Odin Project.
+
+I have used the DOM methods to-
+(i) Create a 16 x 16 grid initially. 
+(ii) Take user input using prompt function.
+(iii) Based on that input creating a new grid in using same amount of pixels.
+(iv) Using rgb values to randomise the background colors.
---
+++ script.js
@@ -0,0 +1,76 @@
+const parentDiv=document.querySelector(".container");
+
+for (let i=0 ; i < 16; i++){
+    const row=document.createElement("div");
+    row.style.display="flex";
+    row.classList.add("row");
+    
+    for (let j=0 ; j<16; j++){
+        const newDiv=document.createElement("div");
+        newDiv.style.backgroundColor='#fefae0';
+        newDiv.style.width="36.25px";
+        newDiv.style.height="36.25px";
+        newDiv.style.border="0.2px solid #4F4E28"
+        newDiv.style.boxSizing="border-box";
+
+        newDiv.addEventListener('mouseout', function (event){
+            newDiv.style.backgroundColor="#4F4E28";
+        });
+
+        row.appendChild(newDiv);
+    }
+    parentDiv.appendChild(row);
+    
+}
+
+function newGrid(event){
+    let input=prompt("Enter the new size(max:100): ");
+
+    numInput=Number(input);
+
+    if (numInput>100 || numInput<0){
+        return alert("Invalid");
+    }
+
+    parentDiv.innerHTML = "";
+
+    for (let i=0 ; i < numInput; i++){
+
+        const row=document.createElement("div");
+        row.style.display="flex";
+        row.classList.add("row");
+        
+        for (let j=0 ; j < numInput; j++){
+
+            const newDiv=document.createElement("div");
+            newDiv.style.backgroundColor = '#fefae0';
+            newDiv.style.width=`${580 / numInput}px`;
+            newDiv.style.height=`${580 / numInput}px`
+            newDiv.style.border="0.2px solid #4F4E28"
+            newDiv.style.boxSizing="border-box";
+    
+            newDiv.addEventListener('mouseout', function (event){
+                newDiv.style.backgroundColor = generateRgb();
+            });
+    
+            row.appendChild(newDiv);
+        }
+        parentDiv.appendChild(row);
+        
+    }
+
+}
+
+function generateRgb() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return `rgb(${red},${green},${blue})`;
+}
+
+document.querySelector("button").addEventListener("click",newGrid);
+
+
+
+
+
